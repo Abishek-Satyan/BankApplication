e196c51c36d43f561c2997a69100c6538bc2e23f
Abishek Satyan <abishek.satyan@gmail.com>
2021-05-25 18:51:54 +0530

    converted to Reactive forms and validations done in registration form and login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
 
@@ -9,15 +10,22 @@ import { DataService } from '../data.service';
 })
 export class LoginComponent implements OnInit {
  min="Your perfect banking partner"
- accno=""
- pswd=""
  
-  constructor(private router:Router,private dataservice:DataService) {  }
+ loginForm=this.fb.group({
+   accno:['',[Validators.required,Validators.pattern('[0-9]*')]],
+   pswd:['',[Validators.required,Validators.minLength(4)]]
+ })
+constructor(private router:Router,private dataservice:DataService,private fb:FormBuilder) {  }
 
   ngOnInit(): void {
   }
 login(){
-  this.dataservice.login(this.accno,this.pswd)
+  if(this.loginForm.valid){
+  this.dataservice.login(this.loginForm.value.accno,this.loginForm.value.pswd)
+  }
+  else{
+    alert("Form not valid")
+  }
 }
 registration(){
   this.router.navigateByUrl("register")
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
 
@@ -10,15 +11,26 @@ import { DataService } from '../data.service';
 })
 export class RegisterComponent implements OnInit {
   min="Your Perfect banking partner";
-  accno="";
-  pswd="";
-  uname="";
-  constructor(private route:Router,private dataservice:DataService) { }
+  
+  registerForm=this.fb.group({
+    uname:['',[Validators.required,Validators.pattern('[a-zA-Z]*')]],
+    accno:['',[Validators.required,Validators.pattern('[0-9]*')]],
+    pswd:['',[Validators.required,Validators.minLength(4)]]
+  })
+  constructor(private route:Router,private dataservice:DataService,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
-  registration(){
-   this.dataservice.registration(this.uname,this.accno,this.pswd)
+  registration(){  
+  if(this.registerForm.get("uname")?.errors){
+    alert("invalid username")
+  }
+  if(this.registerForm.valid){
+   this.dataservice.registration(this.registerForm.value.uname,this.registerForm.value.accno,this.registerForm.value.pswd)
+  }
+  else{
+    alert("form not valid")
+  }
 
   }
   
